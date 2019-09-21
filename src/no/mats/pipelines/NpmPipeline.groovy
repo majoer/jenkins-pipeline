bccfc1c8d0fb9fe4267d0c4a6fa5943a1ccae409
Jørgensen, Mats <matsjoer@gmail.com>
2019-09-21 20:55:58 +0200

    tweak pipeline

--- src/no/mats/pipelines/NpmPipeline.groovy
+++ src/no/mats/pipelines/NpmPipeline.groovy
@@ -1,15 +1,10 @@
 package no.mats.pipelines;
 
 def run(Map<String, String> options) {
-  pipeline {
-
-    stages {
-      stage('Install') {
-        steps {
-          echo 'Install'
-          sh 'npm i'
-        }
-      }
+  stage('Install') {
+    steps {
+      echo 'Install'
+      sh 'npm i'
     }
   }
 }
