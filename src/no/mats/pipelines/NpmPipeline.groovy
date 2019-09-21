d92f5f75365b3f5932c2305ce23cc1e3f701bb44
Jørgensen, Mats <matsjoer@gmail.com>
2019-09-21 20:16:47 +0200

    Use scripted pipeline syntax

--- src/no/mats/pipelines/NpmPipeline.groovy
+++ src/no/mats/pipelines/NpmPipeline.groovy
@@ -3,26 +3,11 @@ package no.mats.pipelines;
 class NpmPipeline {
 
   NpmPipeline(Map<String, String> options) {
-    pipeline {
-      agent any
-      environment {
-        NODE_ENV = 'production'
-      }
 
-      stages {
+    stage ('Install') {
+      echo 'Install'
 
-        stage('Install') {
-          steps {
-            echo 'Install'
-
-            sh 'npm i'
-          }
-        }
-
-        stage('Build') {
-
-        }
-      }
+      sh 'npm i'
     }
   }
 }
