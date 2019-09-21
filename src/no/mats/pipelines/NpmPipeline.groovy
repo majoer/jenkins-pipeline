148e1112685f3dce7a1d8dba5052fcc2b79968d0
Jørgensen, Mats <matsjoer@gmail.com>
2019-09-21 17:30:29 +0200

    Organize libraries

---
+++ src/no/mats/pipelines/NpmPipeline.groovy
@@ -0,0 +1,26 @@
+class NpmPipeline {
+
+  NpmPipeline() {
+    pipeline {
+      agent any
+      environment {
+        NODE_ENV = 'production'
+      }
+
+      stages {
+
+        stage('Install') {
+          steps {
+            echo 'Install'
+
+            sh 'npm i'
+          }
+        }
+
+        stage('Build') {
+
+        }
+      }
+    }
+  }
+}
---
+++ src/no/mats/pipelines/Pipelines.groovy
@@ -0,0 +1,10 @@
+package no.mats.pipelines;
+
+import no.mats.pipelines.NpmPipeline;
+
+class Pipelines {
+
+  static void npmPipeline(options) {
+    new NpmPipeline(options);
+  }
+}
