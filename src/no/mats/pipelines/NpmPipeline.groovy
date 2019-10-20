c57214c77036d5b43b57db6ceb1983cf58f3a011
Jørgensen, Mats <matsjoer@gmail.com>
2019-10-20 19:43:50 +0200

    Use sshagent to push release/beta

--- src/no/mats/pipelines/NpmPipeline.groovy
+++ src/no/mats/pipelines/NpmPipeline.groovy
@@ -105,6 +105,10 @@ def start(Map<String, Object> options = [:]) {
     if (options.deploy && options.deployFromBranch == BRANCH_NAME) {
 
       stage("Deploy to Beta") {
+        sshagent(['git-provider-1']) {
+              sh('git checkout -B release/beta') 
+              sh('git push --set-upstream origin release/beta') 
+          }
       }
     }
   }
