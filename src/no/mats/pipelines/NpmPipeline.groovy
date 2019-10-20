9ca62fe1e080cbe40772b37334398b75b671e255
Jørgensen, Mats <matsjoer@gmail.com>
2019-10-20 17:34:06 +0200

    Remove credential.helper

--- src/no/mats/pipelines/NpmPipeline.groovy
+++ src/no/mats/pipelines/NpmPipeline.groovy
@@ -48,7 +48,7 @@ def start(Map<String, Object> options = [:]) {
         ]) {
         sh 'git config --local user.email "matsjoer@gmail.com"'
         sh 'git config --local user.name "Jenkins"'
-        sh 'git config --local credential.helper "!p() { echo username=$GIT_USERNAME; echo password=$GIT_PASSWORD; }; p"'
+        sh 'git config --local credential.helper ""'
       }
 
       checkout scm
