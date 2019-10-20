24728c5b98e384901190194523f2badd9cd6fe56
Jørgensen, Mats <matsjoer@gmail.com>
2019-10-20 20:07:34 +0200

    Build correct https url

--- src/no/mats/pipelines/NpmPipeline.groovy
+++ src/no/mats/pipelines/NpmPipeline.groovy
@@ -113,10 +113,11 @@ def start(Map<String, Object> options = [:]) {
             )
           ]) {
             def origin = sh(returnStdout: true, script: "git remote get-url origin")
-            def gitUrl = origin.replace("git@", "")
+            def gitUrl = origin.replace("https://", "")
+            def pushUrl = "https://${GIT_USERNAME}:${GIT_PASSWORD}@${gitUrl}"
             
             sh('git checkout -B release/beta') 
-            sh("git push ${GIT_USERNAME}:${GIT_PASSWORD}@${gitUrl}")
+            sh("git push ${pushUrl}")
         }
       }
     }
